1c14f2bfd4d2326d5ac870727685e8c2c2c916ea
Peter Rogers <peter.rogers@gmail.com>
2019-09-29 23:31:34 -0400

    Added version to about page

--- app/src/pages/about/about.ts
+++ app/src/pages/about/about.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Version } from '../../version';
+
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
+
+    get version() {
+        return Version.NUMBER;
+    }
 
 }
